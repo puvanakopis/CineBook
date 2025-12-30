6009677e84e4d05184152acc26ba8bedfe458b09
puvanakopis <puvanakopis@gmail.com>
2025-12-30 21:41:21 +0530

    create footer

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div>
       <Navbar />
       Home
+      <Footer />
     </div>
   );
 }
