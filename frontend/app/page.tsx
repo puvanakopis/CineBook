e867590ba4a17d0b2e65cdc36526881aeb4ce194
puvanakopis <puvanakopis@gmail.com>
2025-12-30 22:07:26 +0530

    create MovieFilter in home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,14 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import HeroSection from "./containers/HeroSection";
+import HeroSection from "@/containers/home/HeroSection";
+import MovieFilter from "@/containers/home/MovieFilter";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <MovieFilter />
       <Footer />
     </div>
   );
