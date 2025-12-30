7e735b82935a07a14185899d96e832abc91dad27
puvanakopis <puvanakopis@gmail.com>
2025-12-30 21:50:25 +0530

    create HeroSection in home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,12 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import HeroSection from "./containers/HeroSection";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      Home
+      <HeroSection />
       <Footer />
     </div>
   );
