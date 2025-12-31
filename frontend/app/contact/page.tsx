882f3fb31f70915fc3402ace9ba3a3dd0003b944
puvanakopis <puvanakopis@gmail.com>
2025-12-31 13:56:37 +0530

    Created the HeroSection in Contact page

---
+++ frontend/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/containers/contact/HeroSection";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <Footer />
+    </div>
+  );
+}
