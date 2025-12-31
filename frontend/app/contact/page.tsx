ee568f2861cf6b7df549214d81832c1f074396fc
puvanakopis <puvanakopis@gmail.com>
2025-12-31 14:24:53 +0530

    add responsive contact form and quick contact info section

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -1,12 +1,14 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/containers/contact/HeroSection";
+import ContactSection from "@/containers/contact/ContactSection";
 
-export default function Home() {
+export default function Contact() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <ContactSection />
       <Footer />
     </div>
   );
