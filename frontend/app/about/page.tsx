7b2fcef45a0df187734497b1b8b9105bbfea0f59
puvanakopis <puvanakopis@gmail.com>
2025-12-31 17:12:49 +0530

    create HeroSection in about page

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import AboutHeroSection from "@/containers/about/AboutHeroSection";
+
+export default function About() {
+  return (
+    <div>
+      <Navbar />
+      <AboutHeroSection />
+      <Footer />
+    </div>
+  );
+}
